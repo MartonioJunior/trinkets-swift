74a5d7d430cb7e3a770062b9f7abec650bd23315
Martônio Júnior <martoniojunior@hotmail.com>
2025-11-28 00:14:30 -0300

    change(Collectables): Unit Tests
    Reworked tests for `Trinketpedia`  and `TrinketRegistry` to not depend on any samples

---
+++ Tests/CollectablesTests/Mock/MockTicket.swift
@@ -0,0 +1,10 @@
+//
+//  MockTicket.swift
+//  Trinkets
+//
+//  Created by Martônio Júnior on 27/11/2025.
+//
+
+import Collectables
+
+public typealias MockTicket = Token<UInt, Bool>
--- Tests/CollectablesTests/Trinketpedia+Tests.swift
+++ Tests/CollectablesTests/Trinketpedia+Tests.swift
@@ -6,18 +6,17 @@
 //
 
 @testable import Collectables
-import SM64Trinkets
 import Testing
 
 // MARK: Mock
 public extension Trinketpedia {
-    static var sm64: Trinketpedia {
+    static var example: Trinketpedia {
         var result = Trinketpedia()
-        result[SM64Cap.self] = .allEntries
-        result[SM64Star.self] = []
-        result[SM64Currency.self] = .allEntries
-        result[SM64Location.self] = []
-        result[SM64KeyItem.self] = []
+        result[MockMaterial.self] = .allEntries
+        result[MockItem.self] = []
+        result[MockCurrency.self] = .allEntries
+        result[MockAccessory.self] = []
+        result[MockTicket.self] = []
         return result
     }
 }
@@ -33,107 +32,108 @@ struct TrinketpediaTests {
     @Test("Returns the database for a registered type")
     func `subscript`() {
         var sut = Trinketpedia()
-        let resultA = sut[SM64Cap.self]
-        let expectedA = SM64Cap.Registry()
+        let resultA = sut[MockMaterial.self]
+        let expectedA = MockMaterial.Registry()
         #expect(resultA == expectedA)
 
-        sut[SM64Cap.self] = [SM64Cap.metal, .vanish]
-        let resultB1 = sut[SM64Cap.self]
-        let resultB2 = sut[SM64Currency.self]
-        let expectedB1: Trinketpedia.Registry = [SM64Cap.vanish, .metal]
-        let expectedB2 = SM64Currency.Registry()
+        sut[MockMaterial.self] = ["lollipop", "cylinder"]
+        let resultB1 = sut[MockMaterial.self]
+        let resultB2 = sut[MockTicket.self]
+        let expectedB1: Trinketpedia.Registry<MockMaterial> = ["lollipop", "cylinder"]
+        let expectedB2 = MockTicket.Registry()
         #expect(resultB1 == expectedB1)
         #expect(resultB2 == expectedB2)
     }
 
     @Test("Retrieves a registered trinket", arguments: [
-        (Trinketpedia(), SM64Cap.wing.id, SM64Cap?.none),
-        (Trinketpedia.sm64, SM64Cap.vanish.id, SM64Cap.vanish),
-        (Trinketpedia.sm64, "Tanooki", SM64Cap?.none)
+        (Trinketpedia(), MockCurrency.coin.id, MockCurrency?.none),
+        (Trinketpedia.example, MockCurrency.feather.id, MockCurrency.feather),
+        (Trinketpedia.example, "Tanooki", MockCurrency?.none)
     ])
-    func `subscript`(_ sut: Trinketpedia, id: SM64Cap.ID, expected: SM64Cap?) {
-        let result = sut[SM64Cap.self, id: id]
+    func `subscript`(_ sut: Trinketpedia, id: MockCurrency.ID, expected: MockCurrency?) {
+        let result = sut[MockCurrency.self, id: id]
         #expect(result == expected)
     }
 
     @Test("Retrieves a registered trinket", arguments: [
-        (Trinketpedia(), "wingCap", SM64Cap?.none),
-        (Trinketpedia.sm64, "vanishCap", SM64Cap.vanish),
-        (Trinketpedia.sm64, "Tanooki", SM64Cap?.none)
+        (Trinketpedia(), "coin", MockCurrency?.none),
+        (Trinketpedia.example, "ticket", MockCurrency.ticket),
+        (Trinketpedia.example, "Tanooki", MockCurrency?.none)
     ])
-    func `subscript`(_ sut: Trinketpedia, dynamicMember member: String, expected: SM64Cap?) {
-        let result: SM64Cap? = sut[dynamicMember: member]
+    func `subscript`(_ sut: Trinketpedia, dynamicMember member: String, expected: MockCurrency?) {
+        let result: MockCurrency? = sut[dynamicMember: member]
         #expect(result == expected)
     }
 
     @Test("Returns a Trinket by it's ID", arguments: [
-        (Trinketpedia(), SM64Cap.wing.id, SM64Cap?.none),
-        (Trinketpedia.sm64, SM64Cap.vanish.id, SM64Cap.vanish),
-        (Trinketpedia.sm64, "Tanooki", SM64Cap?.none)
+        (Trinketpedia(), MockCurrency.ticket.id, MockCurrency?.none),
+        (Trinketpedia.example, MockCurrency.feather.id, MockCurrency.feather),
+        (Trinketpedia.example, "Tanooki", MockCurrency?.none)
     ])
-    func fetch(_ sut: Trinketpedia, id: SM64Cap.ID, expected: SM64Cap?) {
-        let result: SM64Cap? = sut.fetch(id: id)
+    func fetch(_ sut: Trinketpedia, id: MockCurrency.ID, expected: MockCurrency?) {
+        let result: MockCurrency? = sut.fetch(id: id)
         #expect(result == expected)
     }
 
     @Test("Adds new Trinkets to the data structure", arguments: [
-        (Trinketpedia(), SM64Cap.Registry.allEntries, SM64Cap.Registry.allEntries),
-        (Trinketpedia.sm64, SM64Cap.Registry.allEntries, SM64Cap.Registry.allEntries),
-        (Trinketpedia.sm64, SM64Cap.Registry(), SM64Cap.Registry.allEntries),
-        (Trinketpedia(), SM64Cap.Registry(), SM64Cap.Registry())
+        (Trinketpedia(), MockCurrency.Registry.allEntries, MockCurrency.Registry.allEntries),
+        (Trinketpedia.example, MockCurrency.Registry.allEntries, MockCurrency.Registry.allEntries),
+        (Trinketpedia.example, MockCurrency.Registry(), MockCurrency.Registry.allEntries),
+        (Trinketpedia(), MockCurrency.Registry(), MockCurrency.Registry())
     ])
-    func register(_ sut: Trinketpedia, _ database: SM64Cap.Registry, expected: SM64Cap.Registry) {
+    func register(_ sut: Trinketpedia, _ database: MockCurrency.Registry, expected: MockCurrency.Registry) {
         var sut = sut
-        sut.register(SM64Cap.self, database)
-        #expect(sut[SM64Cap.self] == expected)
+        sut.register(MockCurrency.self, database)
+        #expect(sut[MockCurrency.self] == expected)
     }
 
     @Test("Deletes a database from the data structure", arguments: [
-        (Trinketpedia(), true, SM64Cap.Registry(), SM64Currency.Registry()),
-        (Trinketpedia(), false, SM64Cap.Registry(), SM64Currency.Registry()),
-        (Trinketpedia.sm64, true, SM64Cap.Registry.allEntries, SM64Currency.Registry()),
-        (Trinketpedia.sm64, false, SM64Cap.Registry(), SM64Currency.Registry.allEntries)
+        (Trinketpedia(), true, MockCurrency.Registry(), MockMaterial.Registry()),
+        (Trinketpedia(), false, MockCurrency.Registry(), MockMaterial.Registry()),
+        (Trinketpedia.example, true, MockCurrency.Registry.allEntries, MockMaterial.Registry()),
+        (Trinketpedia.example, false, MockCurrency.Registry(), MockMaterial.Registry.allEntries)
     ])
     func removeDatabase(
         _ sut: Trinketpedia,
         currencyOverCaps: Bool,
-        expectedCaps: SM64Cap.Registry,
-        expectedCurrency: SM64Currency.Registry
+        expectedCaps: MockCurrency.Registry,
+        expectedCurrency: MockMaterial.Registry
     ) {
         var sut = sut
 
         if currencyOverCaps {
-            sut.removeDatabase(SM64Currency.self)
+            sut.removeDatabase(MockMaterial.self)
         } else {
-            sut.removeDatabase(SM64Cap.self)
+            sut.removeDatabase(MockCurrency.self)
         }
 
-        #expect(sut[SM64Currency.self] == expectedCurrency)
-        #expect(sut[SM64Cap.self] == expectedCaps)
+        #expect(sut[MockMaterial.self] == expectedCurrency)
+        #expect(sut[MockCurrency.self] == expectedCaps)
     }
 
     @Test("Clears the entire data structure", arguments: [
         (Trinketpedia()),
-        (Trinketpedia.sm64)
+        (Trinketpedia.example)
     ])
     func removeAll(_ sut: Trinketpedia) {
         var sut = sut
         sut.removeAll()
 
-        #expect(sut[SM64Currency.self] == SM64Currency.Registry())
-        #expect(sut[SM64Cap.self] == SM64Cap.Registry())
+        #expect(sut[MockMaterial.self] == MockMaterial.Registry())
+        #expect(sut[MockCurrency.self] == MockCurrency.Registry())
     }
 
     @Test("Defines multiple ways to access a trinket")
     func syntax() {
         let trinketpedia = Trinketpedia()
-        let metalBlockKey = SM64KeyItem.block(.metal).key
-        let metalBlock = trinketpedia[metalBlockKey]
-        let bowserInTheSky: SM64Location.Key = "bowserInTheSky"
-        let capA: SM64Cap? = trinketpedia.fetch(id: "wingCap")
-        let capB: SM64Cap? = trinketpedia[id: "wingCap"]
-        let capC: SM64Cap? = trinketpedia.wingCap
-        let star = trinketpedia[SM64Star.self].toad0
-        let location = trinketpedia[SM64Location.self, id: "bowserSky"]
+        let coinKey = MockAccessory("metalBlock").key
+        let metalBlock = trinketpedia[coinKey]
+        let ringKey: MockItem.Key = "ring"
+        let materialA: MockMaterial? = trinketpedia.fetch(id: "wingFeather")
+        let materialB: MockMaterial? = trinketpedia[id: "wingFeather"]
+        let materialC: MockMaterial? = trinketpedia.wingFeather
+        let dynamicFromKey = trinketpedia[MockAccessory.Key.lostAnkle]
+        let dynamicFromRegistry = trinketpedia[MockMaterial.self].wing
+        let location = trinketpedia[MockTicket.self, id: 10]
     }
 }
