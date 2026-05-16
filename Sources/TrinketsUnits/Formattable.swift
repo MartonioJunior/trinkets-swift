4bb50ce01deb10cea21a5589a10387ccda2063e2
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-15 23:43:11 -0300

    docs(TrinketsUnits): `Formattable`
    Added documentation for the type

--- Sources/TrinketsUnits/Formattable.swift
+++ Sources/TrinketsUnits/Formattable.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
+/// Protocol that allows a value to be formatted based on a given format style.
 public protocol Formattable {
+    /// Formats the object into a given style.
+    /// - Parameter format: Format style to be used.
+    /// - Returns: Output of the formatting operation.
     func formatted<S: FormatStyle>(_ format: S) -> S.FormatOutput where S.FormatInput == Self
 }
 
 // MARK: Default Implementation
 public extension Formattable {
+    // swiftlint:disable:next missing_docs
     func formatted<S: FormatStyle>(_ format: S) -> S.FormatOutput where S.FormatInput == Self {
         format.format(self)
     }
