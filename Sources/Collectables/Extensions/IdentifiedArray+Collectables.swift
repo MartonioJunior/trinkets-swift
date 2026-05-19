52483288b1e18e962e80f4dbb8975dade057ab32
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-19 14:57:52 -0300

    docs(Collectables): Added documentation for the module

--- Sources/Collectables/Extensions/IdentifiedArray+Collectables.swift
+++ Sources/Collectables/Extensions/IdentifiedArray+Collectables.swift
@@ -9,12 +9,14 @@ import IdentifiedCollections
 import SwiftVariety
 
 public extension IdentifiedArray where Element: Identifiable, Element: CaseIterable, ID == Element.ID {
+    /// Identified array with all registered cases for an enum or similar structure.
     static var allCases: Self { .init(Element.allCases) { a, _ in a } }
 }
 
 // MARK: Trinket (EX)
 public extension Trinket {
+    /// Registry used to store trinket entries that are uniquely identified.
     typealias Registry = IdentifiedArrayOf<Self>
-
+    /// Key used to access a trinket registry in an heterogeneous collection.
     static var registryKey: HeterogeneousKey<String, Registry> { .init(trinketpediaID) }
 }
--- Sources/Collectables/Trinket.swift
+++ Sources/Collectables/Trinket.swift
@@ -9,39 +9,68 @@ import SwiftVariety
 
 /// Identifier that uniquely references a Trinket within the scope of a game
 public typealias TrinketKey<T: Trinket> = HeterogeneousKey<T.ID, T>
-
+/// In-game data model that represents something that can be obtained in the game that is uniquely identifiable.
+/// 
+/// Multiple trinket instances can share the same ID.
+/// 
+/// Examples:
+/// ```swift
+/// let rock = Material("rock")
+/// let bossKey = BossKey.forestTemple
+/// let ttm5 = Star.ttm(5)
+/// let ddd1: Star = .ddd(1)
+/// let currency: Currency = .coin
+/// ```
 public protocol Trinket<ID>: Identifiable {
+    /// Identifier associated with the trinket type.
     static var trinketpediaID: Trinketpedia.ID { get }
 }
 
 // MARK: Default Implementation
 public extension Trinket {
+    // swiftlint:disable:next missing_docs
     static var trinketpediaID: Trinketpedia.ID { "\(Self.self)" }
 }
 
 // MARK: Self.Key
 public extension Trinket {
-    public typealias Key = TrinketKey<Self>
-
+    /// Alias for a `TrinketKey`
+    typealias Key = TrinketKey<Self>
+    /// Key used to access this trinket in any heterogeneous collection.
     var key: TrinketKey<Self> { .init(id) }
 }
 
 // MARK: Sequence (EX)
 public extension Sequence where Element == any Trinket {
+    /// Queries for all trinkets of a certain type.
+    /// - Returns: List with all trinkets of type `T`.
     subscript<T: Trinket>(_: T.Type = T.self) -> [T] {
         compactMap { $0 as? T }
     }
 }
 
 public extension Sequence where Element: Trinket {
+    /// Queries for all elements that share an ID with a Trinket Key.
+    /// - Parameter key: Key used.
+    /// - Returns: A list of trinkets with the given id in `key`.
+    /// 
+    /// Example:
+    /// ```swift
+    /// swords[.flameSword]
+    /// ```
     subscript(_ key: TrinketKey<Element>) -> [Element] {
         filter { $0.id == key.id }
     }
-
+    /// Obtains all elements with a given ID.
+    /// - Parameter id: Identifier for the Trinket.
+    /// - Returns:
     subscript(id id: Element.ID) -> [Element] {
         filter { $0.id == id }
     }
-
+    /// Returns all trinkets that are exactly the same as this one.
+    /// - Parameter item: Trinket to evaluate.
+    /// - Returns: List of trinkets equal to `item`.
+    @available(*, deprecated, message: "Use `contains(_:)` instead.")
     subscript(_ item: Element) -> [Element] where Element: Equatable {
         filter { $0 == item }
     }
