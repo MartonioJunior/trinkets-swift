6cd4d752e6d097561fb47b6c806d8e9bc4d66299
Martônio Júnior <martoniojunior@hotmail.com>
2026-06-16 13:00:21 -0300

    docs(Exchange): Added documentation to the module

--- Sources/Exchanges/Trade.swift
+++ Sources/Exchanges/Trade.swift
@@ -5,32 +5,73 @@
 //  Created by Martônio Júnior on 14/07/2025.
 //
 
+/// Exchange performed between two entities, usually a buyer and a seller.
+/// - A: Purchaser of goods.
+/// - B: Seller of goods.
+/// 
+/// Example:
+/// ```swift
+/// Trade {
+///   Exchange {
+///     27 * ironNuggets
+///   } for: {
+///     3 * ironBlocks
+///   }
+/// } seller: {
+///   Exchange {
+///     1 * woodBlock
+///   } for: {
+///     4 * woodPlanks
+///   }
+/// }
+/// ```
 public struct Trade<A: Trader, B: Trader> where A.Buy == B.Sell, B.Buy == A.Sell {
     // MARK: Variables
+    /// Exchange for the purchaser.
     var buyer: ExchangeFor<A>
+    /// Exchange for the seller.
     var seller: ExchangeFor<B>
-
     // MARK: Initializers
+    /// Creates a new trade between traders.
+    /// - Parameters:
+    ///   - buyer: Exchange for the purchaser.
+    ///   - seller: Exchange for the seller.
+    ///
     public init(_ buyer: ExchangeFor<A>, to seller: ExchangeFor<B>) {
         self.buyer = buyer
         self.seller = seller
     }
-
     // MARK: Methods
+    /// Drains resources from purchaser to seller.
+    /// - Parameters:
+    ///   - purchaser: Purchaser of goods.
+    ///   - supplier: Supplier of goods.
+    ///
+    /// - Returns: Remainder of the operation.
     public func drain(from purchaser: inout A?, to supplier: inout B) -> (buyer: A.Sell?, seller: B.Buy?) {
         let buyerRemainder = buyer.drain.apply(to: &purchaser)
         let sellerRemainder = seller.tap.apply(to: &supplier)
 
         return (buyerRemainder, sellerRemainder)
     }
-
+    /// Taps resources into purchaser using seller as the source.
+    /// - Parameters:
+    ///   - purchaser: Purchaser of goods.
+    ///   - supplier: Supplier of goods.
+    ///
+    /// - Returns: Remainder of the operation.
     public func tap(_ purchaser: inout A, using supplier: inout B?) -> (buyer: A.Buy?, seller: B.Sell?) {
         let sellerRemainder = seller.drain.apply(to: &supplier)
         let buyerRemainder = buyer.tap.apply(to: &purchaser)
 
         return (buyerRemainder, sellerRemainder)
     }
-
+    /// Trades resources between purchaser and supplier.
+    /// - Parameters:
+    ///   - purchaser: Purchaser of goods.
+    ///   - supplier: Supplier of goods.
+    ///
+    /// - Returns: Remainder of the operation.
     public func trade(between purchaser: inout A, and supplier: inout B) -> Remainder? {
         let buyerRemainder = buyer.apply(to: &purchaser)
         let sellerRemainder = seller.apply(to: &supplier)
@@ -41,6 +82,7 @@ public struct Trade<A: Trader, B: Trader> where A.Buy == B.Sell, B.Buy == A.Sell
 
 // MARK: Self.Remainder
 public extension Trade {
+    /// Remainder of a trade operation.
     struct Remainder {
         var buyer: ExchangeFor<A>.Remainder
         var seller: ExchangeFor<B>.Remainder?
@@ -48,6 +90,11 @@ public extension Trade {
 }
 
 public extension Trade.Remainder {
+    /// Creates a trade remainder from the exchange remainders.
+    /// - Parameters:
+    ///   - buyer: Remainder from the buyer's exchange.
+    ///   - seller: Remainder from the seller's exchange.
+    ///
     init?(_ buyer: ExchangeFor<A>.Remainder?, seller: ExchangeFor<B>.Remainder?) {
         guard let buyer else { return nil }
 
@@ -67,6 +114,9 @@ extension Trade: Sendable where A: Sendable, B: Sendable, A.Buy: Sendable, A.Sel
 
 // MARK: A.Buy == A.Sell
 public extension Trade where A.Buy == A.Sell {
+    /// Swaps the contents managed by buyer and seller:
+    /// - Buyer now performs the seller's exchange.
+    /// - Seller now performs the buyer's exchange.
     var reversed: Trade<B, A> {
         .init(seller, to: buyer)
     }
@@ -74,10 +124,12 @@ public extension Trade where A.Buy == A.Sell {
 
 // MARK: A == B
 public extension Trade where A == B {
-    static func flow(_ exchange: ExchangeFor<A>) -> Self {
-        .init(exchange, to: exchange.flipped)
-    }
-
+    /// Creates a trade by mapping out an exchange.
+    /// - Parameters:
+    ///   - exchange: Base exchange, used by the buyer.
+    ///   - purchases: Mapper for purchases.
+    ///   - supplies: Mapper for sales.
+    ///
     init(
         _ exchange: ExchangeFor<A>,
         receives purchases: (A.Buy) -> A.Buy,
@@ -85,5 +137,24 @@ public extension Trade where A == B {
     ) {
         self.buyer = exchange
         self.seller = exchange.map(purchases, for: supplies)
+    }
+    /// Creates a trade from a single exchange.
+    /// 
+    /// This is achieved by flipping it for the seller.
+    /// - Parameter exchange: Exchange used as the base.
+    /// - Returns: New trade.
+    /// 
+    /// Example:
+    /// ```swift
+    /// let trade: Trade = .flow {
+    ///   Exchange {
+    ///     50 * leatherPieces
+    ///   } for: {
+    ///     20 * goldenApples
+    ///   }
+    /// }
+    /// ```
+    static func flow(_ exchange: ExchangeFor<A>) -> Self {
+        .init(exchange, to: exchange.flipped)
     }
 }
--- Sources/Exchanges/Transaction.swift
+++ Sources/Exchanges/Transaction.swift
@@ -6,15 +6,33 @@
 //
 
 import Custom
-
+/// Transaction that removes resources from the economy.
 public typealias Drain<Target, Contents> = Transaction<Target?, Contents>
+/// Transaction that adds resources to the economy
 public typealias Tap<Target, Contents> = Transaction<Target, Contents>
-
+/// Modifier that changes the economy in a target.
+/// - Target: Structure being modified.
+/// - Contents: Information about the operation.
+/// 
+/// Example:
+/// ```swift
+/// let addFiveStrawberries = Transaction() {
+///   5 * strawberries
+/// } apply: {
+///   $0.collect($1)
+/// }
+/// ```
 public struct Transaction<Target, Contents> {
+    /// Operation to be performed.
     let apply: @Sendable (inout Target, Contents) -> Contents?
+    /// Contents of the transaction.
     let contents: Contents
-
     // MARK: Initializers
+    /// Creates a new transaction.
+    /// - Parameters:
+    ///   - contents: Contents of the transaction.
+    ///   - apply: Operation to be performed.
+    ///
     public init(
         _ contents: @autoclosure () -> Contents,
         apply: @escaping @Sendable (inout Target, Contents) -> Contents?
@@ -22,8 +40,15 @@ public struct Transaction<Target, Contents> {
         self.contents = contents()
         self.apply = apply
     }
-
     // MARK: Methods
+    /// Creates a new transaction by transforming it's contents.
+    /// - Parameter transform: Function that transforms the content.
+    /// - Returns: Transaction structure with transformed contents.
+    /// 
+    /// Example:
+    /// ```swift
+    /// let giveDoubleStrawberries = giveStrawberries.map { $0 * 2 }
+    /// ```
     public func map(_ transform: (Contents) -> Contents) -> Self {
         .init(transform(contents), apply: apply)
     }
@@ -31,6 +56,7 @@ public struct Transaction<Target, Contents> {
 
 // MARK: DotSyntax
 public extension Transaction {
+    /// Transaction that can operate on optional targets.
     var optional: Transaction<Target?, Contents> {
         let operation = apply
 
@@ -42,11 +68,15 @@ public extension Transaction {
             return result
         }
     }
-    
+    /// Creates a transaction that does not mutate the target.
+    /// - Parameter contents: Contents of the transaction
+    /// - Returns: New transaction.
     static func noop(_ contents: Contents) -> Self {
         .init(contents) { _, contents in contents }
     }
-
+    /// Creates a transaction that nullifies the target, no matter the contents.
+    /// - Parameter contents: Contents of the transaction.
+    /// - Returns: New transaction
     static func nullify(_ contents: Contents) -> Self {
         .init(contents) { _, _ in nil }
     }
@@ -54,6 +84,7 @@ public extension Transaction {
 
 // MARK: Self: Comparable
 extension Transaction: Comparable where Contents: Comparable {
+    // swiftlint:disable:next missing_docs
     public static func < (lhs: Self, rhs: Self) -> Bool {
         lhs.contents < rhs.contents
     }
@@ -61,6 +92,7 @@ extension Transaction: Comparable where Contents: Comparable {
 
 // MARK: Self: Equatable
 extension Transaction: Equatable where Contents: Equatable {
+    // swiftlint:disable:next missing_docs
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.contents == rhs.contents
     }
@@ -68,6 +100,7 @@ extension Transaction: Equatable where Contents: Equatable {
 
 // MARK: Self: Modifier
 extension Transaction: Modifier {
+    // swiftlint:disable:next missing_docs
     public func apply(to target: inout Target) -> Contents? {
         apply(&target, contents)
     }
