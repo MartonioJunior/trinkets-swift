a5a933d168640cf83f0d7a33a208d2c7b19845c7
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-10 20:36:28 -0300

    build: Update "Minimal" to a tagged release

--- Package.swift
+++ Package.swift
@@ -56,7 +56,7 @@ let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/pointfreeco/swift-tagged", .upToNextMajor(from: "0.10.0")),
     .package(url: "https://github.com/apple/swift-numerics", .upToNextMajor(from: "1.1.0")),
     .package(url: "https://github.com/MartonioJunior/Mathe", branch: "main", traits: ["Numerics"]),
-    .package(url: "https://github.com/MartonioJunior/Minimal", branch: "main"),
+    .package(url: "https://github.com/MartonioJunior/Minimal", .upToNextMajor(from: "0.1.0")),
     .package(url: "https://github.com/MartonioJunior/swift-variety", branch: "main"),
     .package(url: "https://github.com/swiftlang/swift-docc-plugin", .upToNextMajor(from: "1.4.5"))
 ]
